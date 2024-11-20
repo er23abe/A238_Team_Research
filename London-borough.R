001d5c1b8f45aef0d6138a5f271208787d110097
Jason Banks <jb21abg@herts.ac.uk>
2024-11-20 14:42:44 +0000

    renamed the columns to be shorter

--- London-borough.R
+++ London-borough.R
@@ -6,4 +6,12 @@ head(dataset,2)
 ds1 <- subset(dataset, Inner..Outer.London != "") # gets rid of blank rows and summary rows.
 
 ds2 <- ds1[c('Area.name','Inner..Outer.London','Population.density..per.hectare..2016','Median.House.Price..2014')]
-# isolates the 4 columns we're interested in.
+# isolates the 4 columns we're interested in. # using https://www.statology.org/r-select-specific-columns/
+
+names(ds2)[1] <- "Borough"
+names(ds2)[2] <- "Location"
+names(ds2)[3] <- "Pop_Density"
+names(ds2)[4] <- "Avg_House_Price"
+
+
+
