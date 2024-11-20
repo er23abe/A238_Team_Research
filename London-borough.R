69e52229458ca5b0471fa43e4758919e7667dbbb
Jason Banks <jb21abg@herts.ac.uk>
2024-11-20 14:35:30 +0000

    Added code to create a new dataset featuring only the columns and rows we are interested in

--- London-borough.R
+++ London-borough.R
@@ -5,5 +5,5 @@ head(dataset,2)
 
 ds1 <- subset(dataset, Inner..Outer.London != "") # gets rid of blank rows and summary rows.
 
-#ds <- subset(dataset$Area.name, dataset$Inner..Outer.London, dataset$Population.density..per.hectare..2016, dataset$Median.House.Price..2014)
-# ^^^ subset gets rid of rows, we need to specify the 4 columns from the powerpoint screenshot - name inner/pouter, median price, pop density. 
+ds2 <- ds1[c('Area.name','Inner..Outer.London','Population.density..per.hectare..2016','Median.House.Price..2014')]
+# isolates the 4 columns we're interested in.
