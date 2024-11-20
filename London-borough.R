12f7081650da07c1edd5b9a9b14b4c43ab1e1418
Jason Banks <jb21abg@herts.ac.uk>
2024-11-20 15:08:16 +0000

    created histograms of population and house price, with different numbers of bars so we can decide which is more useful

--- London-borough.R
+++ London-borough.R
@@ -1,7 +1,7 @@
 
 dataset <- read.csv("london-borough-profiles-2016 Data set.csv")
 
-head(dataset,2)
+#head(dataset,2)
 
 ds1 <- subset(dataset, Inner..Outer.London != "") # gets rid of blank rows and summary rows.
 
@@ -13,5 +13,19 @@ names(ds2)[2] <- "Location"
 names(ds2)[3] <- "Pop_Density"
 names(ds2)[4] <- "Avg_House_Price"
 
+ds2$Pop_Density <- as.numeric(ds2$Pop_Density) # using https://www.statology.org/character-to-numeric-in-r/
+ds2$Avg_House_Price <- as.numeric(gsub(",", "", ds2$Avg_House_Price, fixed = TRUE)) # using https://data-hacks.com/convert-numbers-comma-separator-numeric-r
 
-
+# below using https://herts.instructure.com/courses/116567/files/9935080?module_item_id=4380364 (Practical 3 intro to R).
+h <- hist(ds2$Pop_Density, breaks = 10, xlab = "Population Density (People per Hectare)", ylab = "Frequency", main = "Histogram of Population Density")
+x <- seq(min(ds2$Pop_Density), max(ds2$Pop_Density), length = 100)
+y <- dnorm(x, mean = mean(ds2$Pop_Density), sd = sd(ds2$Pop_Density)) * length(ds2$Pop_Density)
+box.size <- diff(h$mids[1:2])
+y <- y * box.size
+lines(x, y, col = "red") # in hist(), 'breaks' = number of bars in graph
+h <- hist(ds2$Avg_House_Price, breaks = 10, xlab = "Median House Price (£)", ylab = "Frequency", main = "Histogram of Median House Price")
+x <- seq(min(ds2$Avg_House_Price), max(ds2$Avg_House_Price), length = 100)
+y <- dnorm(x, mean = mean(ds2$Avg_House_Price), sd = sd(ds2$Avg_House_Price)) * length(ds2$Avg_House_Price)
+box.size <- diff(h$mids[1:2])
+y <- y * box.size
+lines(x, y, col = "red")
